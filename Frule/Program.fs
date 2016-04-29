b007c2291a0584355c5df827aa70204aed5a89eb
Junle Li <lijunle@gmail.com>
2016-04-30 02:32:15 +0800

    Add the generic timer function to calculate the spend time.

--- Frule/Program.fs
+++ Frule/Program.fs
@@ -1,12 +1,22 @@
 ﻿module Program
 
 open ServiceUrl
+open System
+open System.Diagnostics
+
+let time fn =
+    let watcher = Stopwatch.StartNew()
+    let hashCode = watcher.GetHashCode()
+    printfn "[%d] Start at %A" hashCode DateTime.Now
+    let result = fn ()
+    printfn "[%d] End at %A, Elapsed: %A" hashCode DateTime.Now watcher.Elapsed
+    result
 
 [<EntryPoint>]
 let main argv =
     let email = argv.[0]
     let password = argv.[1]
-    let serviceUrl = getServiceUrl email password
+    let serviceUrl = time (fun () -> getServiceUrl email password)
     match serviceUrl with
     | None -> printfn "Service URL not found!"
     | Some serviceUrl -> printfn "%A" serviceUrl
