1b952d535c8a7992e084f0bf5487752c181633c2
Junle Li <lijunle@gmail.com>
2016-04-30 23:32:26 +0800

    Leverage EWS Manager API to auto discover the endpoint URL.

--- Frule/Program.fs
+++ Frule/Program.fs
@@ -1,7 +1,13 @@
 ﻿module Program
 
+open ServiceUrl
+
 [<EntryPoint>]
 let main argv =
-    printfn "Entry point"
-
+    let email = argv.[0]
+    let password = argv.[1]
+    let serviceUrl = getServiceUrl email password
+    match serviceUrl with
+    | None -> printfn "Service URL not found!"
+    | Some serviceUrl -> printfn "%A" serviceUrl
     0
---
+++ Frule/ServiceUrl.fs
@@ -0,0 +1,23 @@
+﻿module ServiceUrl
+
+open System
+open Microsoft.Exchange.WebServices.Autodiscover
+open Microsoft.Exchange.WebServices.Data
+
+let redirectionUrlValidationCallback (redirectionUrl : string) =
+    let redirectionUri = new Uri(redirectionUrl)
+    let result = redirectionUri.Scheme = "https"
+    result
+
+let autodiscoverServiceUrl email password =
+    try
+        let credential = WebCredentials(email, password)
+        let service = ExchangeService(ExchangeVersion.Exchange2013_SP1, Credentials = credential)
+        let callback = AutodiscoverRedirectionUrlValidationCallback(redirectionUrlValidationCallback)
+        service.AutodiscoverUrl(email, callback)
+        Option.ofObj service.Url
+    with e -> None
+
+let getServiceUrl email password =
+    // TODO read from local cache
+    autodiscoverServiceUrl email password
