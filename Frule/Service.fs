224c4214c65fc215b7f4c01ff48c075bb194ded7
Junle Li <lijunle@gmail.com>
2016-05-15 18:13:56 +0800

    Merge the ServiceUrl module to Service module.

--- Frule/Service.fs
+++ Frule/Service.fs
@@ -1,11 +1,35 @@
 ﻿module Service
 
+open Microsoft.Exchange.WebServices.Autodiscover
 open Microsoft.Exchange.WebServices.Data
+open System
+
+let redirectionUrlValidationCallback =
+    let callback (redirectionUrl : string) =
+        let redirectionUri = Uri redirectionUrl
+        let result = redirectionUri.Scheme = "https"
+        result
+
+    AutodiscoverRedirectionUrlValidationCallback callback
+
+let autodiscoverServiceUrl email (service: ExchangeService) =
+    try
+        service.AutodiscoverUrl(email, redirectionUrlValidationCallback)
+        Result.ret service.Url
+    with e ->
+        Result.Failure e
+
+let getServiceUrl email (service: ExchangeService) =
+    // TODO read from local cache
+    autodiscoverServiceUrl email service
 
 let getService email password : Result<ExchangeService> =
-    let credential = WebCredentials(email, password)
     Result.result {
-        let! url = ServiceUrl.getServiceUrl email password
-        let service = ExchangeService(ExchangeVersion.Exchange2013_SP1, Credentials = credential, Url = url)
+        let credential = WebCredentials(email, password)
+        let service = ExchangeService(ExchangeVersion.Exchange2013_SP1, Credentials = credential)
+
+        let! url = getServiceUrl email service
+        service.Url <- url
+
         return service
     }
