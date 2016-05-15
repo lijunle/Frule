ff6edd32cc5b2851efee4e0948627092064c56d5
Junle Li <lijunle@gmail.com>
2016-05-16 00:36:55 +0800

    Add Frule.Wpf project, initialize main window.

---
+++ Frule.Wpf/Program.fs
@@ -0,0 +1,14 @@
+﻿module Program
+
+open System
+open System.Windows
+
+let getXamlResource xaml =
+    Application.LoadComponent(Uri(xaml, UriKind.Relative))
+    :?> _
+
+[<STAThread>]
+[<EntryPoint>]
+let main args =
+    let mainWindow : Window = getXamlResource "MainWindow.xaml"
+    Application().Run(mainWindow)
