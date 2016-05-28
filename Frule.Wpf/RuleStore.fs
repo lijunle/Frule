44ffe1535f95087e3d7a427dbefcbb9153269002
Junle Li <lijunle@gmail.com>
2016-05-29 04:34:44 +0800

    Minor refine the RuleStore module.

--- Frule.Wpf/RuleStore.fs
+++ Frule.Wpf/RuleStore.fs
@@ -4,17 +4,14 @@ let Zero = {
     Rules = [];
 }
 
-let private comparer (r1 : Rule) (r2 : Rule) =
-    if r1 = r2 then 0 else 1
-
 let compare s1 s2 =
     let list1 = s1.Rules
     let list2 = s2.Rules
-    List.compareWith comparer list1 list2
+    List.compareWith (fun r1 r2 -> if r1 = r2 then 0 else 1) list1 list2
 
 let getDiffRules s1 s2 =
     let list1 = s1.Rules
     let list2 = s2.Rules
     List.zip list1 list2
-    |> List.filter (fun (r1, r2) -> comparer r1 r2 <> 0)
+    |> List.filter (fun (r1, r2) -> r1 <> r2)
     |> List.map (fun (r1, r2) -> r2)
