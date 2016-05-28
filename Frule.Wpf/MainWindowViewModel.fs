6b9fa97a87c0b12c9eb3e46838c23f235cdbed70
Junle Li <lijunle@gmail.com>
2016-05-29 04:22:19 +0800

    Move all events to SuperEvent.

--- Frule.Wpf/MainWindowViewModel.fs
+++ Frule.Wpf/MainWindowViewModel.fs
@@ -42,7 +42,7 @@ type MainWindowViewModel() as this =
     let emptyRule = { Instance = null; Id = null; Name = ""; FolderId = null; FromAddresses = []; SentToAddresses = [] }
     let ruleStore = SuperEvent<RuleStore>(RuleStore.Zero)
     let ruleStoreSaved = SuperEvent<RuleStore>(RuleStore.Zero)
-    let folderSelectedEvent = Event<Folder>()
+    let selectedFolder = SuperEvent<Folder>(loadingFolder)
     let selectedRule = this.SuperEvent<Rule>(emptyRule, <@ this.SelectedRule @>)
     let displayRules = this.SuperEvent<Rule list>([], <@ this.DisplayRules @>)
     let saveEnabled = this.SuperEvent<bool>(false, <@ this.SaveCommand @>)
@@ -81,7 +81,7 @@ type MainWindowViewModel() as this =
     do
         Event.merge
             (ruleStore.Publish |> Event.map RuleStoreUpdated)
-            (folderSelectedEvent.Publish |> Event.map SelectedFolderChagned)
+            (selectedFolder.Publish |> Event.map SelectedFolderChagned)
             |> Event.scan DisplayRule.update DisplayRule.loadingState
             |> Event.map DisplayRule.toList
             |> Event.add (displayRules.Trigger)
@@ -100,6 +100,6 @@ type MainWindowViewModel() as this =
     member this.InboxFolder with get() = [inboxFolder.Value]
     member this.LoginCommand = this.Factory.CommandAsync(login)
     member this.SaveCommand = this.Factory.CommandAsyncChecked(save, fun _ -> saveEnabled.Value)
-    member this.SelectFolderCommand = this.Factory.CommandSyncParam(folderSelectedEvent.Trigger)
+    member this.SelectFolderCommand = this.Factory.CommandSyncParam(selectedFolder.Trigger)
     member this.SelectRuleCommand = this.Factory.CommandSyncParam(selectedRule.Trigger)
     member this.ChangeRuleNameCommand = this.Factory.CommandSyncParam(updateRuleName)
