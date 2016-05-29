03a4fc6acf8f1c01f40f89c5dc031b593e16eded
Junle Li <lijunle@gmail.com>
2016-05-30 01:19:03 +0800

    Remove RuleStore type, which has been replaced by Store.

--- Frule.Wpf/Common/DomainTypes.fs
+++ Frule.Wpf/Common/DomainTypes.fs
@@ -1,10 +1,6 @@
 ﻿[<AutoOpen>]
 module DomainTypes
 
-type RuleStore = {
-    Rules: Rule list;
-}
-
 type Store = {
     Rules': SuperEvent<Rule list>; // TODO pending to change back to Rules
     SavedRules: SuperEvent<Rule list>;
