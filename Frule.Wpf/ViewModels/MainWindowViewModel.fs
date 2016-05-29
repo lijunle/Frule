7781810a9b2183290b3c6cb284da4937ffd41a02
Junle Li <lijunle@gmail.com>
2016-05-30 03:19:50 +0800

    Add UserInfo view model.
    - Move the login and save command to UserInfo view model.
    - Add data context to UserInfo control.
    - The MainWindow trends to be composer.

--- Frule.Wpf/ViewModels/MainWindowViewModel.fs
+++ Frule.Wpf/ViewModels/MainWindowViewModel.fs
@@ -1,37 +1,7 @@
 ﻿namespace ViewModels
 
-open FSharp.ViewModule
-
-type MainWindowViewModel(store : Store) as this =
-    inherit ViewModelBase()
-
-    let currentRules = store.Rules
-    let savedRules = store.SavedRules
-    let saveButtonEnabled = store.SaveButtonEnabled
-
-    let login _ =
-        Views.LoginDialog().ShowDialog() |> ignore
-        User.get () |> Store.loadAsync store
-
-    let save _ = async {
-        do! Async.SwitchToThreadPool () // TODO Make native async operators and avoid this
-
-        let user = User.get ()
-        let modifiedRules = Store.getDiffRules savedRules.Value currentRules.Value
-        Rule.saveToServer user modifiedRules |> ignore
-        savedRules.Trigger currentRules.Value
-    }
-
-    do
-        saveButtonEnabled.Publish.Add(fun _ -> this.RaisePropertyChanged(<@ this.SaveCommand @>))
-
-        SuperEvent.zip savedRules currentRules
-            |> Event.map (fun (v1, v2) -> Store.compare v1 v2 <> 0)
-            |> Event.add (saveButtonEnabled.Trigger)
-
+type MainWindowViewModel(store : Store) =
     member __.InboxFolder = FolderListViewModel store
     member __.DisplayRules = RuleListViewModel store
     member __.SelectedRule = RuleInfoViewModel store
-
-    member this.LoginCommand = this.Factory.CommandAsync(login)
-    member this.SaveCommand = this.Factory.CommandAsyncChecked(save, fun _ -> saveButtonEnabled.Value)
+    member __.UserInfo = UserInfoViewModel store
---
+++ Frule.Wpf/ViewModels/UserInfoViewModel.fs
@@ -0,0 +1,33 @@
+﻿namespace ViewModels
+
+open FSharp.ViewModule
+
+type UserInfoViewModel(store : Store) as this =
+    inherit ViewModelBase()
+
+    let currentRules = store.Rules
+    let savedRules = store.SavedRules
+    let saveButtonEnabled = store.SaveButtonEnabled
+
+    let login _ =
+        Views.LoginDialog().ShowDialog() |> ignore
+        User.get () |> Store.loadAsync store
+
+    let save _ = async {
+        do! Async.SwitchToThreadPool () // TODO Make native async operators and avoid this
+
+        let user = User.get ()
+        let modifiedRules = Store.getDiffRules savedRules.Value currentRules.Value
+        Rule.saveToServer user modifiedRules |> ignore
+        savedRules.Trigger currentRules.Value
+    }
+
+    do
+        saveButtonEnabled.Publish.Add(fun _ -> this.RaisePropertyChanged(<@ this.SaveCommand @>))
+
+        SuperEvent.zip savedRules currentRules
+            |> Event.map (fun (v1, v2) -> Store.compare v1 v2 <> 0)
+            |> Event.add (saveButtonEnabled.Trigger)
+
+    member this.LoginCommand = this.Factory.CommandAsync(login)
+    member this.SaveCommand = this.Factory.CommandAsyncChecked(save, fun _ -> saveButtonEnabled.Value)
