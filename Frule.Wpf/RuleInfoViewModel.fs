b1d2e12ab567d45a4b8b4712eb560b45e077a95b
Junle Li <lijunle@gmail.com>
2016-05-29 17:22:04 +0800

    Add RuleInfoViewModel for view data binding.
    - It is pending to move the operation in view model.

---
+++ Frule.Wpf/RuleInfoViewModel.fs
@@ -0,0 +1,14 @@
+﻿namespace ViewModels
+
+open FSharp.ViewModule
+
+type RuleInfoViewModel(rule : Rule) =
+    inherit ViewModelBase()
+
+    static member Zero = RuleInfoViewModel(Rule.Zero)
+
+    member internal __.Rule = rule
+
+    member __.Name = rule.Name
+    member __.FromAddresses = rule.FromAddresses
+    member __.SentToAddresses = rule.SentToAddresses
